80b25a9ffe818137199645e1aebf4f15ed512ba4
Fijulali <fijulalii@gmail.com>
2025-08-14 11:03:29 +0530

    Initial commit - URL shortener working with UI

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect
+import random, string
+
+app = Flask(__name__)
+
+url_map = {}  # short_code -> original_url
+
+def generate_short_code(length=5):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        long_url = request.form['long_url']
+        short_code = generate_short_code()
+        url_map[short_code] = long_url
+        short_url = request.host_url + short_code
+        return render_template('index.html', short_url=short_url)
+    return render_template('index.html')
+
+@app.route('/<short_code>')
+def redirect_to_url(short_code):
+    long_url = url_map.get(short_code)
+    if long_url:
+        return redirect(long_url)
+    return "URL not found", 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
